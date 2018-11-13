ce84d7a6b1de5707676f95e42039714b2666d51c
Marco-Z <marcozugliani94@live.com>
2018-11-13 12:27:27 +0200

    - support keyboard input (arrow keys) - death if wall is hit

--- kobra.py
+++ kobra.py
@@ -24,8 +24,66 @@ class Kobra():
         self.window.refresh()
 
 
+    def move(self):
+        max_y, max_x = self.window.getmaxyx()
+        head_x, head_y = self.kobra[-1]
+        self.kobra.pop(0)
+        if self.direction == "N":
+            if head_y > 1:
+                self.kobra.append((head_x, head_y - 1))
+            else:
+                self.game_over()
+        elif self.direction == "E":
+            if head_x < max_x - 2:
+                self.kobra.append((head_x + 1, head_y))
+            else:
+                self.game_over()
+        elif self.direction == "S":
+            if head_y < max_y - 2:
+                self.kobra.append((head_x, head_y + 1))
+            else:
+                self.game_over()
+        elif self.direction == "W":
+            if head_x > 1:
+                self.kobra.append((head_x - 1, head_y))
+            else:
+                self.game_over()
+        self.print()
+            
+    def game_over(self):
+        display_string = "GAME OVER!"
+        self.window.erase()
+        self.window.border()
+        max_y, max_x = self.window.getmaxyx()
+        y = max_y // 2
+        x = (max_x - len(display_string)) // 2
+        self.window.addstr(y,x, display_string)
+        self.window.refresh()
+        self.alive = False
+
+    def is_alive(self):
+        return self.alive
+
+    def set_direction(self, key):
+        direction_map = {
+            259: "N", 
+            261: "E", 
+            258: "S", 
+            260: "W",
+        }
+        direction = direction_map.get(key)
+        if direction:
+            self.direction = direction
+
 def main(_, window):
     kobra = Kobra(window)
+    while kobra.is_alive():
+        tic = dt.now()
+        while (dt.now() - tic).microseconds < kobra.speed * 1000:
+            key = window.getch()
+            kobra.set_direction(key)
+        kobra.move()
+
     time.sleep(2)
 
 if __name__ == "__main__":
